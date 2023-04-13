ea24023bad7d0c04cee99e6fe8ed204de7a09e89
shubham vegad <110393780+shubhamVegad@users.noreply.github.com>
2023-04-13 12:07:07 +0530

    delete <button>Search</button>

--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -49,7 +49,6 @@ const HeroBanner = () => {
               onChange={(e) => setQuery(e.target.value)}
               onKeyUp={searchQueryHandler}
             />
-//             <button>Search</button>
           </div>
         </div>
       </ContentWrapper>
