a849af0c510c006891b2b354696ad8bd2dc01e93
shubham vegad <110393780+shubhamVegad@users.noreply.github.com>
2023-04-13 12:02:13 +0530

    Update HeroBanner.jsx

--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -49,7 +49,7 @@ const HeroBanner = () => {
               onChange={(e) => setQuery(e.target.value)}
               onKeyUp={searchQueryHandler}
             />
-            <button>Search</button>
+//             <button>Search</button>
           </div>
         </div>
       </ContentWrapper>
